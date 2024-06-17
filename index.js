6f670e556dfc34e3a348b6f28829a739c932a71f
Julian Londono <julian.londono.9704@gmail.com>
2024-06-17 00:27:13 -0500

    The issues were solved  and the functions were tested from AWS Lambda

--- index.js
+++ index.js
@@ -7,89 +7,123 @@ const pStore = new AWS.SSM();
 const lex = new AWS.LexModelsV2();
 
 AWS.config.update({
-    region: 'us-east-1'
+    region: process.env.region
 });
 /**
  * Updates the required slot in amazon Lex.
  * 
  */
 exports.handler = async () => {
-    var paramsParametersStore = {
+    var secretsParams = {
         Path: process.env.ssmpspath,
         Recursive: false,
         WithDecryption: true
     };
-    var responseParameterStore = await pStore.getParametersByPath(paramsParametersStore).promise();
+
+    // Get the client data from Parameter Store.
+
+    var responseParameterStore = await pStore.getParametersByPath(secretsParams).promise();
+
     var infoParametersStore = responseParameterStore.Parameters;
-    var localParams = {
-        'token': '',
-        'url': '',
-        'siteId': 1
-    };
-    for (let param in localParams) {
-        let paramName = paramsParametersStore.Path + param;
-        let findSecret = infoParametersStore.find(element => element.Name.includes(paramName));
-        if (findSecret) {
-            localParams[param] = findSecret.Value;
-        }
-    }
     var ctx = {
         "api": {
-            "url": localParams.url,
-            "token": localParams.token
+            "url": '',
+            "token": ''
         }
     };
-    
+
+    for (let param in ctx.api) {
+        let paramName = secretsParams.Path + param;
+        let findSecret = infoParametersStore.find(element => element.Name.includes(paramName));
+
+        if (findSecret) {
+            ctx.api[param] = findSecret.Value;
+        }
+    }
+
     // Request the server to get the courses avaibles.
-     
     var courses = await mclient.getCourses(ctx);
-    // Determinate when a course is avaiable.
 
-    // Manage the object to modify the courses with the value visible activated.
-    var courseAvaialbe = courses;
+    // Get the name of the courses in Moodle.
+    var courseAvaialbe = [];
 
-    // ToDo: Determine when a course in the list "courses" is available.
-    // This list is for trying out courses while they are available to read .
-    courseAvaialbe = [
-        "maths",
-        "electronics",
-        "science"
-    ];
+    // Fill courseAvailable array with the courses availables.
+    courses.forEach(element => {
+
+        if (!courseAvaialbe.some(course => course.fullname === element.fullname) && element.showactivitydates) {
+            courseAvaialbe.push(element);
+        }
+    });
 
     // This list is to arrange the courses in the lex API format.
     var slotValues = [];
     courseAvaialbe.forEach(element => {
         let slotName = {
             sampleValue: {
-                value: element
-            }
+                value: element.fullname
+            },
         };
+        if (element.displayname && element.displayname !== element.fullname) {
+            slotName['synonyms'] = [];
+            slotName.synonyms.push({ value: element.displayname });
+
+        }
+        if (element.shortname && element.shortname !== element.fullname) {
+            if (slotName.synonyms && element.shortname !== element.displayname) {
+                slotName.synonyms.push({ value: element.shortname });
+            } else {
+                slotName['synonyms'] = [];
+                slotName.synonyms.push({ value: element.shortname });
+            }
+        }
+
         slotValues.push(slotName);
     });
 
     // Required params to Update Slot via API. Fill It dynamically with the client.
-    const params = {
+    var updateParams =  {
         botId: '',
         botVersion: '',
         localeId: '',
         slotTypeId: '',
-        slotTypeName: process.env.slotname,
+        slotTypeName: 'bbc_courses',
         slotTypeValues: slotValues,
         valueSelectionSetting: {
-            resolutionStrategy: 'OriginalValue',
+            resolutionStrategy: 'TopResolution',
         }
-
     };
-    for (let param in params) {
-        let paramName = paramsParametersStore.Path + param;
+
+    for (let param in updateParams) {
+        let paramName = secretsParams.Path + param;
         let findSecret = infoParametersStore.find(element => element.Name.includes(paramName));
         if (findSecret) {
-            params[param] = findSecret.Value;
+            updateParams[param] = findSecret.Value;
         }
     }
+
     /**
      * Update the slot in Amazon Lex
      */
-    const command = await lex.updateSlotType(params).promise();
-    console.log(command);
+    await lex.updateSlotType(updateParams).promise();
+
+    // Required params to build the bot API. Fill It dynamically with the client.
+    var buildParams = {
+        botId: '',
+        botVersion: '',
+        localeId: ''
+    };
+
+    for (let param in buildParams) {
+        let paramName = secretsParams.Path + param;
+        let findSecret = infoParametersStore.find(element => element.Name.includes(paramName));
+        if (findSecret) {
+            buildParams[param] = findSecret.Value;
+        }
+    }
+
+    /**
+     * Build the bot after update the slot.
+     */
+
+    lex.buildBotLocale(buildParams).promise();
 };
